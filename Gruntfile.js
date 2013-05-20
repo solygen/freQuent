6c1d496d19e463975f04518ae808a5cb10ade148
Frank Paczynski <frank.paczynski@solygen.de>
2013-05-20 13:51:25 +0200

    fixed grunt settings; added repository property to package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,17 @@
 //keep jshint happy
 var module = module || {};
 
+//versioning
+//http://datasift.github.io/gitflow/Versioning.html
+
 module.exports = function (grunt) {
 
     'use strict';
 
     // Initializes the Grunt tasks with the following settings
     grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
 
         //export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
         jsdoc : {
@@ -55,16 +60,20 @@ module.exports = function (grunt) {
         // Files to be concatenated (source and destination files)
         concat: {
             js: {
-                src: ['lib/*.js'],
-                dest: 'lib/freQuent.js'
+                src: ['lib/!(frequent*).js'],
+                dest: '<%= pkg.name %>.<%= pkg.version %>.js'
             }
         },
 
         //and minified (source and destination files)
         uglify: {
-            dist: {
-                src: ['<%= concat.js.dest %>'],
-                dest: 'lib/freQuent.min.js'
+            options: {
+                banner: '/* <%= pkg.name %>.<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>) <%= pkg.repository.url %> */'
+            },
+            my_target: {
+                files: {
+                    '<%= pkg.name %>.<%= pkg.version %>.min.js': ['<%= concat.js.dest %>']
+                }
             }
         },
 
@@ -83,5 +92,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-jsdoc');
 
     // if grint is called without any further parameter
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jsdoc', 'jshint', 'jsdoc', 'concat', 'uglify']);
 };
