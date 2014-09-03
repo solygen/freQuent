fd647be49997f179175a2e5faed86a62c253abbb
solygen <frank.paczynski@solygen.de>
2014-09-03 06:13:12 +0200

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,40 +13,10 @@ module.exports = function (grunt) {
 
         pkg: grunt.file.readJSON('package.json'),
 
-        //export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
-        jsdoc : {
-            dist : {
-                src: ['lib/*.js'],
-                options: {
-                    destination: 'doc'
-                }
-            }
-        },
-
         // A list of files, which will be syntax-checked by JSHint
         jshint: {
             files: ['Gruntfile.js', 'lib/*.js'],
-        },
-
-        // Files to be concatenated (source and destination files)
-        concat: {
-            js: {
-                src: ['lib/!(frequent*).js'],
-                dest: '<%= pkg.name %>.<%= pkg.version %>.js'
-            }
-        },
-
-        //and minified (source and destination files)
-        uglify: {
-            options: {
-                banner: '/* <%= pkg.name %>.<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>) <%= pkg.repository.url %> */'
-            },
-            my_target: {
-                files: {
-                    '<%= pkg.name %>.<%= pkg.version %>.min.js': ['<%= concat.js.dest %>']
-                }
-            }
-        },
+        }
 
         watch: {
             files: '<%= jshint.files %>',
@@ -57,12 +27,8 @@ module.exports = function (grunt) {
 
     // Load the plugins
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jsdoc');
 
     // if grint is called without any further parameter
-    grunt.registerTask('default', ['jsdoc', 'jshint', 'jsdoc', 'concat', 'uglify']);
-    grunt.registerTask('build', ['concat', 'uglify']);
+    grunt.registerTask('default', ['jshint']);
 };
